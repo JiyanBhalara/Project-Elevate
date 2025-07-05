b5a3a79fd68c543e0e67009c8a0ef856abd700f3
jiyan_bhalara <jiyanbhalara8806@gmail.com>
2025-07-05 16:52:45 -0400

    main.ts updated with enabling CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://project-elevate-web.vercel.app',
+    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type','Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,          // strips unknown properties
