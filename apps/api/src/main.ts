4acd6f15bdab8b63d52b2ab122ac17279d90246a
jiyan_bhalara <jiyanbhalara8806@gmail.com>
2025-07-05 15:58:10 -0400

    main.ts updated

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: /vercel\.app$/ });
+  app.enableCors("http://localhost:4000");
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,          // strips unknown properties
