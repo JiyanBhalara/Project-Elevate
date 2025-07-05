dc9a1fafc4e423dc001bd7a7cc8dc201034558b0
jiyan_bhalara <jiyanbhalara8806@gmail.com>
2025-07-05 11:26:51 -0400

    /api/index.ts was created for deployment

---
+++ apps/api/api/index.ts
@@ -0,0 +1,20 @@
+import serverlessExpress from '@vendia/serverless-express';
+import { Handler } from 'aws-lambda';
+import express from 'express';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from 'src/app.module';
+
+let cached: Handler | undefined;
+
+async function bootstrap(): Promise<Handler> {
+  const app = express();
+  const nest = await NestFactory.create(AppModule, new ExpressAdapter(app));
+  await nest.init();
+  return serverlessExpress({ app });
+}
+
+export const handler: Handler = async (event, ctx, cb) => {
+  cached ??= await bootstrap();
+  return cached(event, ctx, cb);
+};
